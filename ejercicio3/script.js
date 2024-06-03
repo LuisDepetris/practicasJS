ff985e73093baad7d4bfdab332b2a09b7d325224
Luis Depetris <depetrisluis@gmail.com>
2024-06-03 11:25:57 -0300

    Agrego ejercicio 4

--- ejercicio3/script.js
+++ ejercicio3/script.js
@@ -1,5 +1,14 @@
+function calculoPerimetro() {
+  const base = parseFloat(document.getElementById("base").value)
+  const altura = parseFloat(document.getElementById("altura").value)
+  alert(`El perimetro total es: ${(base*2)+(altura*2)}`)
+ }
+ 
+ function calculoArea(){
+    const base = parseFloat(document.getElementById("base").value)
+    const altura = parseFloat(document.getElementById("altura").value)
+    alert(`El Area total es: ${base*altura}`)
+ }
+
 window.onload = () => {
-  document.getElementById("texto").addEventListener("input", () => {
-      parrafo.innerHTML = document.getElementById("texto").value;
-  })
 }
