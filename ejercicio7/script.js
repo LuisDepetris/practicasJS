a767e7080420a1c3924e813542da587f6eedb8a0
Luis Depetris <depetrisluis@gmail.com>
2024-06-04 12:20:30 -0300

    Agrego ejercicio 7

---
+++ ejercicio7/script.js
@@ -0,0 +1,8 @@
+const nombres = ["Luis","Jose", "Tiago", "Benja"]
+const listado = document.getElementById('listado')
+
+for(let i = 0; i < nombres.length; i++){
+  const listadoHijo = document.createElement('li');
+  listadoHijo.textContent = nombres[i];
+  listado.appendChild(listadoHijo);
+}
