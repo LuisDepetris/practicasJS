80f83651cc5ccca8f218bcc92142f8060156045e
Luis Depetris <depetrisluis@gmail.com>
2024-06-04 12:24:39 -0300

    pruebo con foreach

--- ejercicio7/script.js
+++ ejercicio7/script.js
@@ -1,8 +1,14 @@
 const nombres = ["Luis","Jose", "Tiago", "Benja"]
 const listado = document.getElementById('listado')
 
-for(let i = 0; i < nombres.length; i++){
+// for(let i = 0; i < nombres.length; i++){
+//   const listadoHijo = document.createElement('li');
+//   listadoHijo.textContent = nombres[i];
+//   listado.appendChild(listadoHijo);
+// }
+
+nombres.forEach(function(elemento){
   const listadoHijo = document.createElement('li');
-  listadoHijo.textContent = nombres[i];
+  listadoHijo.textContent = elemento;
   listado.appendChild(listadoHijo);
-}
+})
